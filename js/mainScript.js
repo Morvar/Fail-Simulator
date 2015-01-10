c401e9e64409b682f32d6508da102eab32cd4f4b
sofia.larborn@gmail.com <Morvar>
2015-01-10 01:57:58 +0100

    Started constructing renderer

--- js/mainScript.js
+++ js/mainScript.js
@@ -1,4 +1,21 @@
 //scene setup
 var scene = new THREE.Scene();
-//create perspective camera(field of view, aspect ratio[width/height of element],near, far [clipping plane. no rendering nearer than near or beyond far. improves performance])
-var camera = new THREE.PerspectiveCamera();
+//create perspective camera(FOV field of view [degrees], 
+//aspect ratio[width/height of element],near, far [clipping plane. 
+//no rendering nearer than near or beyond far. improves performance])
+var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+//create renderer. set render size to browser window size
+var renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+//append the renderer as a child element to the body of the HTML document.
+//three.js creates a canvas inside body element for rendering the scene
+document.body.appendChild(renderer.domElement); //domElement???----
+
+//the rendering function
+function render(){
+    requestAnimationFrame(render); 
+    renderer.render(scene, camera); 
+} 
+render();
+
+scene.add(camera);
