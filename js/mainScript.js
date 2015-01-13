a81302e391b0035e0aaa31d5cb20029d3281426a
sofia.larborn@gmail.com <Morvar>
2015-01-13 22:59:16 +0100

    jQuery and more init()
    Added jQuery library to folder and index.html, worked more on init()
    function.

--- js/mainScript.js
+++ js/mainScript.js
@@ -1,18 +1,7 @@
-//scene setup
-var scene = new THREE.Scene();
+//
 var movespeed = 100, lookSpeed = 0.075;
-var camera, controls, renderer;
-//create renderer. set render size to browser window size
-renderer = new THREE.WebGLRenderer();
-renderer.setSize(window.innerWidth, window.innerHeight);
-//change background canvas color
-renderer.domElement.style.backgroundColor = '#85D6FF';
-//append the renderer as a child element to the body of the HTML document.
-//three.js creates a canvas inside body element for rendering the scene
-document.body.appendChild(renderer.domElement); //domElement???-----
+var scene, camera, controls, renderer, playerHeight;
 
-//track position of cursor (event type, function to execute, true/false:capturing/bubbling)
-document.addEventListener('mousemove', onDocumentMouseMove, false);
 
 //the rendering function
 function render(){
@@ -20,23 +9,41 @@ function render(){
     renderer.render(scene, camera);
 } 
 render();
-
+//_________________________________________________
 
 function init(){
+    //scene setup - creating the world. the scene has all other objects
+    scene = = new THREE.Scene();
+    
     //create perspective camera(FOV field of view [degrees], 
     //aspect ratio[width/height of element],near, far [clipping plane. 
     //no rendering nearer than near or beyond far. improves performance])
     camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-    camera.position.y = playerSize * 0.7; //set camera height
+    camera.position.y = playerHeight * 0.7; //set camera height position
     scene.add(camera);
 
-    //FirstPersonControls: Camera moves with mouse, player moves using WASD/arrow keys
-    //takes object as argument
+    //FirstPersonControls: move camera with mouse, player using WASD/arrow keys
+    // takes the camera object as argument
 	controls = new THREE.FirstPersonControls(camera);
+    controls.lookSpeed = lookSpeed; //player look around speed (mouse)
+    controls.lookVertical = false; //player can't look up/down (prevents flying)
 	controls.moveSpeed = moveSpeed; //player move around speed
-	controls.lookSpeed = lookSpeed; //player look around speed (mouse)
-	controls.lookVertical = false; //player can't look up/down (prevents flying)
-	controls.noFly = true; //no using R/F for moving up/down
+	controls.noFly = true; //no using R/F keys for moving up/down
     
     sceneSetup(); //-----------
+    
+    //create renderer. set render size to browser window size
+    renderer = new THREE.WebGLRenderer();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    //change background canvas color
+    renderer.domElement.style.backgroundColor = '#85D6FF';
+    //append the renderer as a child node to the body of the HTML document.
+    //three.js creates a canvas inside body element for rendering the scene
+    document.body.appendChild(renderer.domElement); //domElement???-------
+    
+    //Attaches an event handler to the element 'document'
+    //track position of cursor (event type,function to execute,
+    //,true/false:capturing/bubbling = 'mousemove' will trigger onDocumentMouseMove 
+    // on innermost element and bubble up to parents)
+    document.addEventListener('mousemove', onDocumentMouseMove, false);
 }
