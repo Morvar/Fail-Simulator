4092416c93ebb8457b519a4498705822f1d43bcd
sofia.larborn@gmail.com <Morvar>
2015-01-16 15:21:46 +0100

    Added map, clock, projector, units

--- js/mainScript.js
+++ js/mainScript.js
@@ -1,6 +1,26 @@
+
 //Global variables
-var moveSpeed = 100, lookSpeed = 0.075;
-var scene, camera, controls, renderer, unitSize, wallheight;
+var moveSpeed = 200, 
+    lookSpeed = 0.1, 
+    unitSize = 200, 
+    wallheight = 100, 
+    mouse = {x: 0, y: 0};
+var width = window.innerWidth, height = window.innerHeight, aspect = width/height;
+var scene, units, camera, controls, renderer, clock, projector;
+
+var map =[//0  1  2  3  4  5  6  7  8  9
+           [1, 1, 1, 1, 1, 1, 1, 1, 1, 1], // 0
+           [1, 0, 0, 0, 0, 0, 0, 0, 0, 0], // 1
+           [1, 0, 0, 0, 0, 0, 0, 0, 0, 1], // 2
+           [1, 0, 0, 0, 0, 0, 0, 0, 0, 1], // 3
+           [1, 0, 0, 0, 0, 0, 0, 0, 0, 1], // 4
+           [1, 0, 0, 0, 0, 0, 0, 0, 0, 1], // 5
+           [1, 0, 0, 0, 0, 0, 0, 0, 0, 1], // 6
+           [1, 0, 0, 0, 0, 0, 0, 0, 0, 1], // 7
+           [1, 0, 0, 0, 0, 0, 0, 0, 0, 1], // 8
+           [1, 1, 1, 1, 1, 1, 1, 1, 1, 1], // 9
+           ], 
+            mapWidth = map[0].length, mapHeight = map.length;
 
 //the rendering function
 function render(){
@@ -8,13 +28,17 @@ function render(){
     //collision check, player location and stuff??
     
     //argument callback
-    requestAnimationFrame(render); //-----
+    requestAnimationFrame(render); //-------
     renderer.render(scene, camera);
 }
-render();
+//render();
+
 //_________________________________________________
 
 function init(){
+    
+    clock = new THREE.Clock(); //calculate time between rendering frames
+	projector = new THREE.Projector(); //helper class for projecting. makes 2D into 3D rays
     //scene setup - creating the world. the scene holds the other objects
     scene = new THREE.Scene();
     
@@ -52,19 +76,20 @@ function init(){
 }
 
 function sceneSetup(){
-    //create the floor of the map
-    var floor = //??
-    scene.add(floor);
     
-    //
+    var units = mapWidth;
+        
+    //create the floor of the map
     //BoxGeometry(width, height, depth, widthSegments, heightSegments, depthSegments)
+    var floor = new THREE.Mesh(
+        new THREE.BoxGeometry(units * unitSize, 10, units * unitSize), 
+        new THREE.MeshLambertMaterial({color: 0xEDCBA0}));
+    scene.add(floor);
+
     var cube = new THREE.BoxGeometry(unitSize, wallHeight, unitSize); 
-    var material = new THREE.MeshBasicMaterial({color: 0x00ff00});
-    var wallCube = new THREE.Mesh(cube, material); 
+    var wallMaterial = new THREE.MeshBasicMaterial({color: 0x00ff00});
+    var wallCube = new THREE.Mesh(cube, wallMaterial);
     scene.add(wallCube);
     
     //light ??
 }
-
-
-
