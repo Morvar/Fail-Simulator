eb6aed90c56262e88b1da0c81d8c685d31475ca4
sofia.larborn@gmail.com <Morvar>
2015-01-17 20:57:48 +0100

    More work on init() and sceneSetup()
    started constructing for loop for rendering the map. moved render()
    function to bottom

--- js/mainScript.js
+++ js/mainScript.js
@@ -10,7 +10,7 @@ var scene, units, camera, controls, renderer, clock, projector;
 
 var map =[//0  1  2  3  4  5  6  7  8  9
            [1, 1, 1, 1, 1, 1, 1, 1, 1, 1], // 0
-           [1, 0, 0, 0, 0, 0, 0, 0, 0, 0], // 1
+           [1, 0, 0, 0, 0, 0, 0, 0, 0, 1], // 1
            [1, 0, 0, 0, 0, 0, 0, 0, 0, 1], // 2
            [1, 0, 0, 0, 0, 0, 0, 0, 0, 1], // 3
            [1, 0, 0, 0, 0, 0, 0, 0, 0, 1], // 4
@@ -21,17 +21,6 @@ var map =[//0  1  2  3  4  5  6  7  8  9
            [1, 1, 1, 1, 1, 1, 1, 1, 1, 1], // 9
            ], 
             mapWidth = map[0].length, mapHeight = map.length;
-
-//the rendering function
-function render(){
-
-    //collision check, player location and stuff??
-    
-    //argument callback
-    requestAnimationFrame(render); //-------
-    renderer.render(scene, camera);
-}
-//render();
 
 //_________________________________________________
 
@@ -46,7 +35,7 @@ function init(){
     //aspect ratio[width/height of element],near, far [clipping plane. 
     //no rendering nearer than near or beyond far. improves performance])
     camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 1000);
-    camera.position.y = unitSize * 0.7; //set camera height position
+    camera.position.y = unitSize * 0.5; //set camera height position
     scene.add(camera);
 
     //FirstPersonControls: move camera with mouse, player using WASD/arrow keys
@@ -57,39 +46,70 @@ function init(){
 	controls.moveSpeed = moveSpeed; //player move around speed
 	controls.noFly = true; //no using R/F keys for moving up/down
     
-    sceneSetup(); //----------- function to be created
+    sceneSetup(); //call function to set up the environment
     
     //create renderer. set render size to browser window size
     renderer = new THREE.WebGLRenderer();
-    renderer.setSize(window.innerWidth, window.innerHeight);
+    renderer.setSize(width, height);
+    
     //change background canvas color
     renderer.domElement.style.backgroundColor = '#85D6FF';
     //append the renderer as a child node to the body of the HTML document.
     //three.js creates a canvas inside body element for rendering the scene
-    document.body.appendChild(renderer.domElement); //domElement???-------
+    document.body.appendChild(renderer.domElement); //add to document body
     
     //Attaches an event handler to the element 'document'
     //track position of cursor (event type,function to execute,
     //,true/false:capturing/bubbling = 'mousemove' event will trigger onDocumentMouseMove 
     // on innermost element and bubble up to parents)
     document.addEventListener('mousemove', onDocumentMouseMove, false);
+    
+    //shooting???
 }
 
 function sceneSetup(){
     
-    var units = mapWidth;
+    var units = mapWidth; 
         
     //create the floor of the map
     //BoxGeometry(width, height, depth, widthSegments, heightSegments, depthSegments)
+    //Mesh(geometry, material)
+    //MeshLambertMaterial(properties of parameters object)
     var floor = new THREE.Mesh(
         new THREE.BoxGeometry(units * unitSize, 10, units * unitSize), 
-        new THREE.MeshLambertMaterial({color: 0xEDCBA0}));
+        new THREE.MeshLambertMaterial({color: 0x00ff00}) );
     scene.add(floor);
 
     var cube = new THREE.BoxGeometry(unitSize, wallHeight, unitSize); 
-    var wallMaterial = new THREE.MeshBasicMaterial({color: 0x00ff00});
-    var wallCube = new THREE.Mesh(cube, wallMaterial);
-    scene.add(wallCube);
+    var wallMaterial = new THREE.MeshLambertMaterial({color: 0x00ff00});
     
-    //light ??
+    //loopa igenom map och placera wallcubes ------------------
+    // 
+    for(i = 0; i < mapWidth; i++){
+        for(j = 0; l = map[i].length; j < l){
+            if(){
+                var wallCube = new THREE.Mesh(cube, wallMaterial);
+                scene.add(wallCube);
+            }
+        }
+    }
+    
+    //light
+    //DirectionalLight(hex, intensity)
+    var direcLight = new THREE.DirectionalLight(0x00ff00, 0.5);
+    //set position of light source
+	direcLight.position.set(1, 1, 1);
+	scene.add(direcLight);
 }
+
+//the rendering function
+function render(){
+
+    //collision check, player location and stuff??
+    
+    //argument callback
+    requestAnimationFrame(render); //-------
+    renderer.render(scene, camera);
+    
+}
+//render();
